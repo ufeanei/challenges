3402db5e194da4ee83af1d93fd5cfd1da7a30b5b
ufeanei <akaranjoh@yahoo.com>
2015-12-15 07:44:55 +0100

    handshake and robot simulator challenge solution completed

---
+++ handshake.rb
@@ -0,0 +1,54 @@
+require 'pry'
+
+class SecrethHandshake
+  attr_accessor :bin
+
+  def initialize(num)
+     @bin = num.to_i.to_s == '0' ? [] : num.to_i.to_s(2) # guard against 0, letter strings
+  end
+
+  def wrong_input_message
+  	puts "Input must be decimal number or decimal number string excluding zero"
+  end
+
+  def commands
+  	return wrong_input_message if bin == []
+  	
+  	handshake = []
+  	revbin = bin.reverse # using reverse! here produces weird results from line 45 to 48
+    handshake << 'wink' if revbin[0] =='1'
+    handshake << 'double blink' if revbin[1] == '1'
+    handshake << 'close your eyes' if revbin[2] == '1'
+    handshake << 'jump' if revbin[3] == '1'
+    handshake = ['jump', 'close your eyes', 'double blink','wink'] if revbin[4] == '1'
+
+    handshake
+    
+  end
+end
+
+
+c =SecrethHandshake.new(2)
+
+puts c.bin
+
+puts "#{c.commands}"
+
+b = SecrethHandshake.new('4')
+
+puts b.bin
+puts "#{b.commands}"
+
+a = SecrethHandshake.new(0)
+
+puts a.bin
+puts "#{a.commands}"
+puts "#{a.commands}"
+puts "#{a.commands}"
+puts "#{a.commands}"
+
+binding.pry
+  
+  	
+  
+  
---
+++ robotsim.rb
@@ -0,0 +1,101 @@
+require 'pry'
+
+class Robot
+  attr_accessor :xcor, :ycor, :orientation
+
+  def at(x,y)
+  	@xcor = x
+  	@ycor = y
+  end
+
+  def orient(direction)
+  	if [:north, :east, :west, :south].include?(direction.to_sym)
+  	  @orientation = direction.to_sym
+    else
+  	  raise "Drection must be east, west, south, north"
+    end
+  end
+
+  def bearing
+  	orientation
+  end
+
+  def coordinates
+  	[xcor, ycor]
+  end
+
+  def turn_right
+  	self.orientation = :north if bearing == :west
+  	self.orientation = :east  if bearing == :north
+  	self.orientation = :south if bearing == :east
+  	self.orientation = :west  if bearing == :south
+  end
+
+  def turn_left
+  	self.orientation = :north if bearing == :east
+  	self.orientation = :east  if bearing == :south
+  	self.orientation = :south if bearing == :west
+  	self.orientation = :west  if bearing == :north
+  end
+
+  def advance
+  	self.xcor += 1 if bearing == :east
+  	self.xcor -= 1 if bearing == :west
+  	self.ycor += 1 if bearing == :north
+  	self.ycor -= 1 if bearing == :south
+  end
+
+end
+
+
+class Simulator
+
+  def place(rob, x, y, direction )
+    rob.at(x,y)
+    rob.orient(direction)
+  end
+
+  def wrong_command_message(let)
+  	puts "#{let.upcase} is not a valid robot command" if !['L', 'A', 'R'].include?(let.upcase)
+  end
+
+
+  def evaluate(rob, instruct) 
+    inst = instruct.upcase.split(//)
+    inst.each do |letter|
+  	  wrong_command_message(letter) # robot throws an error for invalid commands and execute valid commands
+  	  rob.turn_left if letter == 'L'
+  	  rob.turn_right if letter == 'R'
+  	  rob.advance if letter == 'A'
+    end
+  end
+
+  def instruction(input)
+  	commands = []
+  	n = input.size
+  	for i in 0...n
+
+  	  commands << :turn_right if input[i].upcase == 'R'
+  	  commands << :turn_letf if input[i].upcase == 'L'
+  	  commands << :advance if input[i].upcase == 'A'
+  	  wrong_command_message(input[i])
+  	end
+  	commands
+  end
+
+end
+
+
+robot1 = Robot.new
+simulator = Simulator.new
+simulator.place(robot1, 0, 0, 'west')
+
+puts " enter your robot instruction"
+answer = gets.chomp
+puts "#{simulator.instruction(answer)}"
+simulator.evaluate(robot1, answer)
+puts "#{robot1.bearing}"
+puts "#{robot1.coordinates}"
+
+
+
