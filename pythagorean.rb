3109beb64f0d13433bf99231656a60db5ae6c98a
ufeanei <akaranjoh@yahoo.com>
2015-12-16 00:16:04 +0100

    added code for pythagorean tirplet challenge

---
+++ pythagorean.rb
@@ -0,0 +1,50 @@
+class Triplet
+	attr_accessor :a, :b, :c
+
+	def self.where(input)
+		max = input[:max_factor]
+		min = input[:min_factor] || 1
+		sum = input[:sum]
+
+		triplets = []
+
+		num_arr = (min..max).to_a
+		num_arr.combination(3) do |comb|
+			triple = Triplet.new(comb[0], comb[1], comb[2])
+			if sum 
+				triplets << triple if triple.pythagorean? && triple.sum == sum
+			else
+				triplets << triple if triple.pythagorean? 
+			end
+		end
+		triplets
+	end
+
+	def initialize(a, b, c)
+		@a = a
+		@b = b
+		@c = c
+	end
+
+	def sum
+		a + b + c
+	end
+
+	def product
+		a*b*c
+	end
+
+	def pythagorean?
+		a*a + b*b == c*c
+	end
+end
+
+z = Triplet.where(max_factor: 100, sum: 180)
+
+ a = z.map(&:product)
+
+# puts "#{}"
+
+puts "#{a}"
+
+		
