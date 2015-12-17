eb59fb8f5b7528b2d907333b6beff2daaa38c43d
ufeanei <akaranjoh@yahoo.com>
2015-12-17 14:23:56 +0100

    still on indentation

--- octal.rb
+++ octal.rb
@@ -1,24 +1,23 @@
-
 class Octal
-	attr_accessor :octnum
+  attr_accessor :octnum
 
-	def initialize(num)
-		@octnum = num
-	end
+  def initialize(num)
+    @octnum = num
+  end
 
-	def not_valid?
-		 octnum == '0' || octnum =~ /[A-Fa-f8-9]/
-	end
+  def not_valid?
+     octnum == '0' || octnum =~ /[A-Fa-f8-9]/
+  end
 
-	def to_decimal
-		sum = 0
-		return sum if not_valid?
-		oct_reverse = octnum.reverse
-		for i in 0...oct_reverse.size
-			sum += oct_reverse[i].to_i* 8**i
-		end
-		sum
-	end
+  def to_decimal
+    sum = 0
+    return sum if not_valid?
+    oct_reverse = octnum.reverse
+    for i in 0...oct_reverse.size
+      sum += oct_reverse[i].to_i* 8**i
+    end
+    sum
+  end
 end
 
 
